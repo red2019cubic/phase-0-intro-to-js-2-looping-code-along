b289d8ccb2b0776545355c7ad3f18a7aa5351e0a
Red <red.sarouf2014@gmail.com>
2023-03-05 15:46:41 -0500

    Added loop assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const array = ['Guadalupe', 'Ollie', 'Aki']
+function writeCards(array, name){
+    for(let i = 0; i < array.length; i++){
+        const message = `Thank you, ${array[i]}, for the wonderful ${name} gift!`
+        array[i] = message;
+    }
+    return array;
+}
+
+function countDown(number){
+    let count = 0;
+    while(count <= number){
+        console.log(count);
+        count++;
+    }
+}
